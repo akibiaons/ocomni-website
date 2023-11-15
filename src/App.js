9abfef2f0837639f2552d8d74db31831d482f5a1
Arthur Fernandez <arthur@snooget.com>
2023-11-15 06:41:00 -0800

    Delete blog, will use strapi instead

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const Home = lazy(() => import("./Pages/Home"));
-const Blog = lazy(() => import("./Pages/Blog"));
 
 const Header = lazy(() => import("./components/Header/index"));
 const Footer = lazy(() => import("./components/Footer/index"));
@@ -18,7 +17,6 @@ function App() {
           <Header />
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/blog" element={<Blog />} />
           </Routes>
           <Footer />
           <ScrollToTop />
