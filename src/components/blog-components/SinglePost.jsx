74b75231051eef78fa345f0a32b0dddd495854b8
Arthur Fernandez <arthur@snooget.com>
2023-11-18 13:45:45 -0800

    Update to article styling with style components

--- src/components/blog-components/SinglePost.jsx
+++ src/components/blog-components/SinglePost.jsx
@@ -1,6 +1,43 @@
 import moment from "moment";
 import React from "react";
 import Comments from "./Comments";
+import styled from "styled-components";
+
+const StyledArticle = styled.article`
+  margin: auto;
+  margin-top: 3.5rem;
+  margin-bottom: 3.5rem;
+  max-width: 40rem; /* equivalent to max-w-2xl */
+  padding-left: 2rem;
+  padding-right: 2rem;
+`;
+
+const AuthorImage = styled.img`
+  margin-right: 1rem;
+  width: 4rem; /* equivalent to w-16 */
+  height: 4rem; /* equivalent to h-16 */
+  border-radius: 9999px; /* equivalent to rounded-full */
+`;
+
+const AuthorName = styled.a`
+  font-size: 1.25rem; /* equivalent to text-xl */
+  font-weight: 600; /* equivalent to font-semibold */
+  color: #1f2937; /* equivalent to text-gray-900 */
+  transition: color 100ms ease-in-out;
+  &:hover {
+    color: #6b7280; /* equivalent to hover:text-gray-500 */
+  }
+  cursor: pointer;
+`;
+
+const PostTitle = styled.h1`
+  font-weight: bold;
+  font-size: 2.25rem; /* equivalent to text-4xl */
+  line-height: 1.25; /* equivalent to leading-snug */
+  @media (min-width: 768px) {
+    line-height: 1; /* equivalent to md:leading-none */
+  }
+`;
 
 const Post = ({ post }) => {
   console.log(post);
@@ -72,66 +109,49 @@ const Post = ({ post }) => {
     }
   };
   return (
-    <article className="mx-auto my-14 max-w-2xl px-8">
-      {/* File path Component */}
+    <StyledArticle>
+      <article className="mx-auto my-14 max-w-2xl px-8">
+        {/* File path Component */}
 
-      {/* Author Component */}
-      <div>
-        <header class="mb-4 lg:mb-6 not-format">
-          <address class="flex items-center mb-6 not-italic">
-            <div class="inline-flex items-center mr-3 text-sm text-gray-900">
-              <img
-                class="mr-4 w-16 h-16 rounded-full"
-                src={post.author.photo.url}
-                alt={`Photograph of author ${post.author.name}`}
-              />
-              <div className="font">
-                <a
-                  href="#"
-                  rel="author"
-                  class="text-xl font-semibold text-gray-900 transition duration-100 ease-in-out hover:text-gray-500 cursor-pointer"
-                >
-                  {post.author.name}
-                </a>
-                <p class="md:text-base font-primarybody text-gray-500 text-xl ">
-                  {post.author.roles}
-                </p>
-                <p class="md:text-base  font-primarybody text-gray-500 text-xl">
-                  {moment(post.createdAt).format("MM/DD/YYYY")}
-                </p>
+        {/* Author Component */}
+        <div>
+          <header>
+            <address>
+              <div>
+                <AuthorImage
+                  src={post.author.photo.url}
+                  alt={`Photograph of author ${post.author.name}`}
+                />
+                <AuthorName href="#">{post.author.name}</AuthorName>
+
+                <p>{moment(post.createdAt).format("MM/DD/YYYY")}</p>
               </div>
-            </div>
-          </address>
-        </header>
-      </div>
+            </address>
+          </header>
+        </div>
 
-      {/* post */}
-      <h1 className="font-bold text-4xl leading-snug md:leading-none">
-        {post.title}
-      </h1>
-      <br />
-      <p className="font-normal text-lg leading-relaxed selection:bg-purple-500 text-gray-700 text-inherit">
-        {post.excerpt}
-      </p>
-      <img src={post.featuredImage.url} className="my-6 w-[90%] mx-auto" />
+        {/* post */}
+        <PostTitle>{post.title}</PostTitle>
+        <br />
+        <p>{post.excerpt}</p>
+        <img src={post.featuredImage.url} />
 
-      {post.content.raw.children.map((typeObj, index) => {
-        const children = typeObj.children.map((item, itemindex) =>
-          getContentFragment(itemindex, item.text, item)
-        );
+        {post.content.raw.children.map((typeObj, index) => {
+          const children = typeObj.children.map((item, itemindex) =>
+            getContentFragment(itemindex, item.text, item)
+          );
 
-        return (
-          <p className="text-lg leading-relaxed text-gray3-700">
-            {getContentFragment(index, children, typeObj, typeObj.type)}
-          </p>
-        );
-      })}
+          return (
+            <p>{getContentFragment(index, children, typeObj, typeObj.type)}</p>
+          );
+        })}
 
-      {/* will work on comments later */}
-      <div>
-        <Comments />
-      </div>
-    </article>
+        {/* will work on comments later */}
+        <div>
+          <Comments />
+        </div>
+      </article>
+    </StyledArticle>
   );
 };
 
